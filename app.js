473b5313895d8a5f5046fe6021dab33af9c9065d
David Washington <david.washington@microsoft.com>
2017-09-17 11:47:39 -0500

    working draft

--- app.js
+++ app.js
@@ -6,16 +6,53 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 
-app.get('/', function(req, res) {
- res.sendFile(__dirname + '/public/default.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/default.html');
 
 });
 
-io.on('connection', function(socket) {
-    console.log('new connection ' + socket);
-           
+io.on('connection', function (socket) {
+    socket.on('join', function (room) {
+        console.log('Join room ' + room);
+
+        var numClients = io.sockets.adapter.rooms[room] == undefined ? 0 : io.sockets.adapter.rooms[room].length;
+        console.log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+        if (numClients === 0) {
+            socket.join(room);
+            console.log('Client ID ' + socket.id + ' created room ' + room);
+            socket.emit('created', room, socket.id);
+
+        } else if (numClients === 1) {
+            console.log('Client ID ' + socket.id + ' joined room ' + room);
+            io.sockets.in(room).emit('join', room);
+            socket.join(room);
+            socket.emit('joined', room, socket.id);
+            io.sockets.in(room).emit('ready');
+        } else { // max two clients
+            socket.emit('full', room);
+        }
+    });
+
+    socket.on('offer', (offer) => {
+        console.log('offer: ' + offer);
+        socket.broadcast.emit('offer', offer);
+    });
+
+    socket.on('answer', (answer) => {
+        console.log('answer: ' + answer);
+        
+        socket.broadcast.emit('answer', answer);
+    })
+
+    socket.on('candidate', (candidate)  => {
+        console.log('candidate: ' + candidate);
+        
+        socket.broadcast.emit('candidate', candidate);        
+    });
+
 });
 
-http.listen(port, function() {
+http.listen(port, function () {
     console.log('listening on *: ' + port);
 });
--- public/default.js
+++ public/default.js
@@ -1,10 +1,17 @@
 'use strict';
 
+var isInitiator;
+var socket = io.connect();
+
+var video = document.querySelector('.localVideo');
+var remoteVideo = document.querySelector('.remoteVideo');
+
+var joinButton = document.querySelector('.joinButton');
+var callButton = document.querySelector('.callButton');
+
 ///////////////////////////////
 // Step 1: Get the webcam video
 ///////////////////////////////
-
-var video = document.querySelector('video');
 
 var constraints = {
   audio: false,
@@ -12,12 +19,133 @@ var constraints = {
 };
 
 navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-    window.stream = stream; // stream available to console
-    if (window.URL) {
-      video.src = window.URL.createObjectURL(stream);
-    } else {
-      video.src = stream;
-    }
+  window.localStream = stream; // stream available to console
+  if (window.URL) {
+    video.src = window.URL.createObjectURL(stream);
+  } else {
+    video.src = stream;
+  }
+
+  socket.emit('message', 'mediaReady');
+  
+  peerConnection.addStream(stream);
+
 }, (err) => {
-    console.log('navigator.getUserMedia error: ', error);    
+  console.log('navigator.getUserMedia error: ', error);
 })
+
+///////////////////////////////
+// Step 2: Join a room
+///////////////////////////////
+
+joinButton.addEventListener('click', (e) => {
+  var room = "David"
+  socket.emit('join', room);
+})
+
+socket.on('created', (room, clientId) => {
+  isInitiator = true;
+  console.log('Created room: ' + room)
+  
+});
+
+socket.on('joined', (room, clientId) => {
+  console.log('Joined room: ' + room);
+
+  isInitiator = false;
+});
+
+
+
+socket.on('full', (room) => {
+  console.log('Room: ' + room + ' is full');
+});
+
+
+/////////////////////////////////////
+// Step 3: Create a peer connection
+/////////////////////////////////////
+
+var servers = {}
+
+var peerConnection = new RTCPeerConnection(servers);
+console.log('Created local peer connection object');
+
+peerConnection.onaddstream = () => {
+  console.log('Remote stream added.');
+  remoteVideo.src = window.URL.createObjectURL(event.stream);
+  window.remoteStream = event.stream;
+};
+
+peerConnection.onremovestream = () => {
+  console.log('Remote stream removed. Event: ', event);
+};
+
+peerConnection.onicecandidate = (e) => {
+  console.log('onIceCandidate');
+
+  if (e.candidate) {
+    console.log('candidate: ' + e.candidate);
+
+    socket.emit('candidate',
+    {
+      type: 'candidate',
+      label: event.candidate.sdpMLineIndex,
+      id: event.candidate.sdpMid,
+      candidate: event.candidate.candidate
+    });
+  }
+};
+
+
+/////////////////////////////////////
+// Step 4: Initiate the call
+/////////////////////////////////////
+
+callButton.addEventListener('click', (e) => {
+  if (isInitiator) {
+    peerConnection.createOffer((sessionDescription) => {
+      peerConnection.setLocalDescription(sessionDescription);
+  
+      socket.emit('offer', sessionDescription);
+  
+    }, (error) => {
+      console.log('Create offer error: ' + error);
+  
+    });
+  }
+})
+
+socket.on('offer', (offer) => {
+
+  peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+  
+  //answer
+  peerConnection.createAnswer().then((sessionDescription) => {
+      peerConnection.setLocalDescription(sessionDescription);
+      socket.emit('answer', sessionDescription);
+    },
+    (error) => {
+      console.log('Answer Error:'+ error);
+    }
+  );
+
+})
+
+socket.on('candidate', (candidate) => {
+  var iceCandidate = new RTCIceCandidate({
+    sdpMLineIndex: candidate.label,
+    candidate: candidate.candidate
+  });
+  peerConnection.addIceCandidate(iceCandidate);
+})
+
+/////////////////////////////////////
+// Step 4: Answer the call
+/////////////////////////////////////
+
+socket.on('answer', (answer) => {
+  peerConnection.setRemoteDescription(new RTCSessionDescription(answer));  
+})
+
+
